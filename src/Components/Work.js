acd3a286598bfeffe56fde17014e7752073c777e
Jesus Garcia Moreno <jesus2208f@gmail.com>
2019-12-27 22:57:25 -0500

    Update information

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -24,7 +24,11 @@ function Work() {
             <Col xs={12} md={10}>
               <div>
                 <ul>
-                  <li>To be determined...</li>
+                  <li>Architected and developed a full-stack application with MongoDB, ExpressJS, React, and NodeJS which would track scheduled outages for a variety of major services used by the Claims System Transformation team</li>
+                  <li>Contributed to a health-check app in React and C# that monitors the performance of microservices through API endpoints, which served as a test result and visualization service to reduce debugging time from hours to seconds</li>
+                  <li>Integrated email notifications to alert the responsible developers about the failed test run against their service</li>
+                  <li>Leveraged the Jira API and internal SQL databases utilizing NodeJS to predict the estimated date of completion for multiple project versions through a linear regression model using story points and status assigned to each issue</li>
+                  <li>Configured a CI/CD pipeline using Jenkins to compile and deploy the application source code to PCF</li>
                 </ul>
               </div>
             </Col>
@@ -42,8 +46,9 @@ function Work() {
               <div>
                 <ul>
                   <li>Built automation scripts using Python to improve the test cycle by reducing the time spent on manual testing</li>
-                  <li>Worked closely with Development and QA teams to plan and execute tests across mobile, server-client, RESTful APIs, SQL databases, and hardware devices to ensure it meets the specified requirements</li>
                   <li>Kickstarted the automation test suite for the BioConnect Android mobile app using the Appium-Python client</li>
+                  <li>Significantly contributed to the Postman API validation test suite by adding over 75 end-to-end test cases, resulting in approximately a total of 1000 API requests with assertions written in JavaScript</li>
+                  <li>Worked closely with Development and QA teams to plan and execute tests across mobile, server-client, RESTful APIs, SQL databases, and hardware devices to ensure it meets the specified requirements</li>
                   <li>Identified, analyzed, and implemented appropriate automation technology for respective platforms</li>
                 </ul>
               </div>
