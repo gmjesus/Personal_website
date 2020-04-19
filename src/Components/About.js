f91767e163fd045c4dd81e8733f1952ae795c721
Jesus Garcia Moreno <jesus2208f@gmail.com>
2020-04-18 22:16:01 -0400

    small changes to contact page

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,12 +9,12 @@ function About() {
       <Card className="vertical-center" id="about">
         <Card.Body>
         <Row>
-          <Col xs={12} lg={4} style={{textAlign: "center"}}>
+          <Col xs={12} lg={4} className="text-center">
             <Image src={photo} fluid as="h1" className="profile-img" title="Jesus Garcia Moreno" alt="Jesus Garcia Moreno"/>
           </Col>
           <Col xs={12} lg={8}>
-            <Card.Title as="h1" style={{textAlign: "center"}}>Jesus Garcia Moreno</Card.Title>
-            <Card.Subtitle as="h2" style={{textAlign: "center"}} className="text-muted">Computer Engineering Student</Card.Subtitle>
+            <Card.Title as="h1" className="text-center">Jesus Garcia Moreno</Card.Title>
+            <Card.Subtitle as="h2" className="text-center text-muted">Computer Engineering Student</Card.Subtitle>
             <hr />
             <Card.Text>
               <span>I'm a second-year computer engineering student studying at the <a rel="noopener noreferrer" href="https://uwaterloo.ca/" target="_blank">University of Waterloo</a>. On my free time, I'm usually watching some Netflix and anime, keeping up with soccer, attending hackathons and tech challenges, or hanging out with friends!</span>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import { Card, Container, Button, ButtonGroup } from 'react-bootstrap';
+import { Card, Container, Button, ButtonGroup, Row, Col } from 'react-bootstrap';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -8,14 +8,24 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 function Contact() {
   return (
     <Container>
-      <Card className="text-center" id="contact">
+      <Card id="contact">
         <Card.Body>
-          <Card.Title as="h2">Contact Me!</Card.Title>
-            <ButtonGroup size="lg">
-              <Button variant="dark" href="mailto:jf2garci@edu.uwaterloo.ca"><EmailIcon fontSize="large"/></Button>
-              <Button variant="dark" href="https://www.linkedin.com/in/jesus-garciam/"><LinkedInIcon fontSize="large"/></Button>
-              <Button variant="dark" href="https://github.com/gmjesus"><GitHubIcon fontSize="large"/></Button>
-            </ButtonGroup>
+          <Card.Title as="h2">Contact</Card.Title>
+          <hr />
+          <Row>
+            <Col xs={12} md={6}>
+              <Card.Text style={{marginBottom: "8px"}}>
+                <span>Want to chat or learn more about me? Feel free to use my contact information on this page!</span>
+              </Card.Text>
+            </Col>
+            <Col xs={12} md={6} className="text-center">
+              <ButtonGroup size="lg">
+                <Button title="Email" variant="dark" href="mailto:jf2garci@uwaterloo.ca"><EmailIcon fontSize="large"/></Button>
+                <Button title="LinkedIn" variant="dark" href="https://www.linkedin.com/in/jesus-garciam/"><LinkedInIcon fontSize="large"/></Button>
+                <Button title="GitHub" variant="dark" href="https://github.com/gmjesus"><GitHubIcon fontSize="large"/></Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
         </Card.Body>
         <Card.Footer style={{textAlign: 'center'}} className="text-muted">&copy; 2020 Jesus Garcia Moreno. All Rights Reserved.</Card.Footer>
       </Card>
