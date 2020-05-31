634a10fc77c81616199d1e188c4a5b8054903129
Jesus Garcia Moreno <jesus2208f@gmail.com>
2020-05-31 14:12:31 -0400

    changes to education page

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -10,7 +10,7 @@ export default function Education() {
   }, [])
 
   const data = {
-    courses_link: "http://www.ucalendar.uwaterloo.ca/2021/COURSE/course-ECE.html",
+    ece_courses_link: "http://www.ucalendar.uwaterloo.ca/2021/COURSE/course-ECE.html",
     courses: [
       { subject: "ECE", number: 108, title: "Discrete Mathematics and Logic 1"},
       { subject: "ECE", number: 124, title: "Digital Circuits and Systems"},
@@ -28,14 +28,15 @@ export default function Education() {
           <hr />
           <Card.Img className="image" src={photo} title="University of Waterloo" alt="University of Waterloo"/>
           <Card.Subtitle as="h4">Univeristy of Waterloo - Waterloo, ON, Canada</Card.Subtitle>
-          <div>Bachelor of Applied Science - BASc, 2018-2023</div>
-          <div>Program: Computer Engineering, Honours, Co-op</div>
+          <div>Degree: BASc - Bachelor of Applied Science</div>
+          <div>Program: <a href="https://uwaterloo.ca/electrical-computer-engineering/" rel="noopener noreferrer" target="_blank">Computer Engineering, Co-op</a></div>
+          <div>Expected Graduation Date: May 2023</div>
           <br />
           <div>Listed below are some of my favourite courses I completed during my undergrad:</div>
           <ListGroup horizontal='lg' className="my-2">
             {data.courses.map((course, key) => {
               return (
-                <ListGroup.Item key={key} action href={data.courses_link + "#" + course.subject + course.number} target="_blank" rel="noopener noreferrer">
+                <ListGroup.Item key={key} action href={data.ece_courses_link + "#" + course.subject + course.number} target="_blank" rel="noopener noreferrer">
                   {course.subject} {course.number} - {course.title}
                 </ListGroup.Item>
               )
